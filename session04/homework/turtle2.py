c95477a86e6e55da4e539a8b9a9dd31a80acd7e1
phanhung2905 <41588583+phanhung2905@users.noreply.github.com>
2018-07-31 23:15:54 +0700

    Update turtle2.py

--- session04/homework/turtle2.py
+++ session04/homework/turtle2.py
@@ -6,18 +6,17 @@ color("blue")
 
 
 right(130)
-x = 40
 y = 200
 
 
 
-for i in range(x):
+for i in range(40):
     
     for k in range(4):
         forward(y)
         right(90)
         
-    right(360/x)
+    right(360/40)
     y -= 5
 
 mainloop()
