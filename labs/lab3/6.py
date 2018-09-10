6fdeb0fae78c7eca8f333480eee0ed5be1940efc
phanhung2905 <hunghanh2905@gmail.com>
2018-09-11 04:59:24 +0700

    btvn

---
+++ labs/lab3/6.py
@@ -0,0 +1,14 @@
+from turtle import *
+from stars import draw_star
+
+speed(-1)
+color("blue")
+for i in range(100):
+    import random
+    x = random.randint(-300, 300)
+    y = random.randint(-300, 300)
+    length = random.randint(3, 10)
+    draw_star(x, y, length)
+
+
+
---
+++ labs/lab3/stars.py
@@ -0,0 +1,13 @@
+def draw_star (x, y, length):
+    import turtle
+    
+    turtle.penup()
+    turtle.goto(x, y)
+    turtle.pendown()
+        
+    
+    for k in range (5):
+        turtle.forward(length)
+        turtle.right(144)
+        
+    turtle.mainloop()
